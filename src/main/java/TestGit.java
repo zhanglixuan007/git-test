1444341178833f1b1df1b8f32bfc2590f78de3ab
zlx <zhanglx0425@163.com>
2022-07-19 16:57:38 +0800

    update .gitignore

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -3,5 +3,6 @@ public class TestGit {
         System.out.println("hello,git");
         System.out.println("hello,git1");
         System.out.println("hello,git2");
+        System.out.println("hello,git3");
     }
 }
